6cccfac72bd6ca3190bd38357c62c49fb1597e8d
Kristin Aldridge <kristinaldridge202@gmail.com>
2024-07-30 22:29:42 -0500

    fixed folder structure

--- server/routes/bookings.mjs
+++ server/routes/bookings.mjs
@@ -4,18 +4,7 @@ import Booking from '../models/bookingModel.js';
 
 const router = express.Router();
 const uri = process.env.ATLAS_URI || "mongodb+srv://kristinaldridge202:PEbkTw1g3Q646hEW@cluster13.wgztqop.mongodb.net/booking_data?retryWrites=true&w=majority&appName=Cluster13";
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
 
-// let db;
-// client.connect().then(() => {
-//   db = client.db('bookings');
-//   console.log('Connected to MongoDB');
-// }).catch(err => console.error('Failed to connect to MongoDB', err));
-
-// await db.collection('bookings').find({}).toArray();
 
 // GET 
 router.get('/', async (req, res) => {
@@ -43,8 +32,7 @@ router.post('/', async (req, res) => {
 
   try {
     const result = Booking.create(booking)
-    // await db.collection('bookings').insertOne(booking);
-    // res.status(201).json(result.ops[0]);
+   
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
@@ -55,11 +43,7 @@ router.delete('/:id', async (req, res) => {
   try {
       await Booking.findByIdAndDelete(req.params.id);
 
-    // if (result.deletedCount > 0) {
-    //   res.json({ message: 'Booking deleted' });
-    // } else {
-    //   res.status(404).json({ message: 'Booking not found' });
-    // }
+   
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
